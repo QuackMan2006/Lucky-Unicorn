c8d531b08e6b3277b28a48342a537e6648f38e4e
QuackMan2006 <98863726+QuackMan2006@users.noreply.github.com>
2022-04-08 17:24:02 +1200

    Create 01_yes_no_function1.py

---
+++ 01_yes_no_function1.py
@@ -0,0 +1,34 @@
+""" LU yes/no
+puts code created in v2 into a loop to make testing easier and more
+efficient.
+"""
+
+
+# functions go here...
+def yes_no(question_text):
+    while True:
+
+        # ask user if they've played before
+        answer = input(question_text).lower()
+
+        # if they say yes, output 'program continues'
+        if answer == "yes" or answer == "y":
+            answer = "Yes"
+            return answer
+
+        # if they say no, output 'display instructions'
+        elif answer == "no" or answer == "n":
+            answer = "No"
+            return answer
+
+        # otherwise - show error
+        else:
+            print("Please answer 'yes or 'no'")
+
+
+# main routine go here...
+show_instructions = yes_no("Have you played this game before? ")
+print(f"You entered '{show_instructions}'")
+print()
+having_fun = yes_no("Are you having fun? ")
+print(f"You entered '{having_fun}'")
